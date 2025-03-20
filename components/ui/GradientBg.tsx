bb5057107cefc3dd121cd52a40ca372b5495c321
hardikvyazz <hardikvyas.dev@gmail.com>
2025-03-20 18:30:37 +0530

    test

--- components/ui/GradientBg.tsx
+++ components/ui/GradientBg.tsx
@@ -40,6 +40,9 @@ export const BackgroundGradientAnimation = ({
   const [tgX, setTgX] = useState(0);
   const [tgY, setTgY] = useState(0);
   useEffect(() => {
+
+    if(typeof window === "undefined") return;
+
     document.body.style.setProperty(
       "--gradient-background-start",
       gradientBackgroundStart
